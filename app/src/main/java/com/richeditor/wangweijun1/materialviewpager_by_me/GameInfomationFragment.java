6f9c505885a2b28254026c2ed47a707345c63518
wangweijun1 <wangweijun1@le.com>
2018-01-03 18:35:04 +0800

    show ui

--- app/src/main/java/com/richeditor/wangweijun1/materialviewpager_by_me/GameInfomationFragment.java
+++ app/src/main/java/com/richeditor/wangweijun1/materialviewpager_by_me/GameInfomationFragment.java
@@ -3,12 +3,15 @@ package com.richeditor.wangweijun1.materialviewpager_by_me;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.github.florent37.materialviewpager.header.MaterialViewPagerHeaderDecorator;
 
 /**
  * Created by wangweijun1 on 2018/1/2.
@@ -21,34 +24,47 @@ public class GameInfomationFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View homeView = View.inflate(getActivity(), R.layout.fragment_game_infomation, null);
-        ListView listView = (ListView)homeView.findViewById(R.id.listView);
-        listView.setAdapter(new GameInfomationAdapter());
+        RecyclerView recyclerView = (RecyclerView)homeView.findViewById(R.id.recycler_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.addItemDecoration(new MaterialViewPagerHeaderDecorator());
+        recyclerView.setAdapter(new GameInfomationAdapter());
         return homeView;
     }
 
-    class GameInfomationAdapter extends BaseAdapter {
+    class GameInfomationAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
 
         @Override
-        public int getCount() {
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_rank, parent, false);
+            return new MyViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            BaseModel baseModel = new BaseModel();
+            baseModel.name = "position:"+position;
+            holder.nameTextView.setText(baseModel.name);
+            holder.itemView.setTag(baseModel);
+        }
+
+        @Override
+        public int getItemCount() {
             return 20;
         }
 
-        @Override
-        public Object getItem(int i) {
-            return null;
-        }
 
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
+    }
 
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            TextView tv = new TextView(getActivity().getBaseContext());
-            tv.setText("positon:"+i);
-            tv.setTextSize(40);
-            return tv;
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+        ImageView avatarView;
+        TextView nameTextView;
+        TextView emailTextView;
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            nameTextView = itemView.findViewById(R.id.text_name);
         }
     }
 
